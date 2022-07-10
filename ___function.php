6c8f5fbd2108b5eca963dd5af4ded143d1b17b7f
Sanjay Kumar <60152138+sanjayskd@users.noreply.github.com>
2022-07-11 04:14:58 +0530

    Update ___function.php

--- ___function.php
+++ ___function.php
@@ -5,7 +5,7 @@ $allowed_image_extension = array("png","jpg","jpeg","PNG","JPEG","JPG");
 						if($_FILES["background_image"]["name"] != ''){
 							$file_extension = explode(".",$_FILES["background_image"]["name"]);
 								$new_name  = date(time()).rand(1,99999);
-								$background_image = 'uploads/blogs/'.$new_name. $_FILES['background_image']['name'];     
+								$background_image = 'uploads/'.$new_name. $_FILES['background_image']['name'];     
 								move_uploaded_file($_FILES['background_image']['tmp_name'], $background_image);
 						}else{
 							$background_image = $_POST['background_imagehidden'];
